8071a4a9c87a707559b71b4d64e7378213c307d8
DiegoMariucciScuola <diego.mariucci@iisgubbio.edu.it>
2022-03-05 15:49:56 +0100

    primo commit di prova

--- src/it/edu/iisgubbio/GameFight/Main.java
+++ src/it/edu/iisgubbio/GameFight/Main.java
@@ -1,5 +1,33 @@
 package it.edu.iisgubbio.GameFight;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+	Pane areaDiGioco = new Pane();
+	
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+    	areaDiGioco.setPrefSize(400, 400);
+        
+        Image iBase = new Image(getClass().getResourceAsStream("/res/bg/bg1.gif"));
+        ImageView asteroide = new ImageView(iBase);
+        asteroide.setPreserveRatio(true);
+        asteroide.setFitWidth(500);
+        areaDiGioco.getChildren().add(asteroide);
+        
+        
+        
+        Scene scena = new Scene(areaDiGioco);
+        primaryStage.setScene(scena);
+        primaryStage.setTitle("CD");
+        primaryStage.show();
+	}
+	public static void main(String[] args) {
+        launch(args);
+    }
 }
