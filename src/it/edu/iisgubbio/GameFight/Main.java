d45af11cfcfdebab273b813a8492ff426377e5e4
DiegoMariucciScuola <diego.mariucci@iisgubbio.edu.it>
2022-03-05 16:42:35 +0100

    aggiunte quasi tutte le gif di axel e dr

--- src/it/edu/iisgubbio/GameFight/Main.java
+++ src/it/edu/iisgubbio/GameFight/Main.java
@@ -5,19 +5,38 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	Pane areaDiGioco = new Pane();
 	
+	Rectangle rettangolo = new Rectangle(180,249);
+
+	
 	@Override
     public void start(Stage primaryStage) throws Exception {
         
+		//imposto lo sfondo
         Image iBase = new Image(getClass().getResourceAsStream("bg1.gif"));
         ImageView sfondo = new ImageView(iBase);
         sfondo.setPreserveRatio(true);
         sfondo.setFitWidth(1000);
         areaDiGioco.getChildren().add(sfondo);
+        
+        
+        //Image Base = new Image(getClass().getResourceAsStream("player1.gif"));
+        //rettangolo.setImage(Base);
+        //Image img = new Image("player1.gif");
+        Image img = new Image(getClass().getResourceAsStream("player1.gif"));
+        //ImageView player1 = new ImageView(img);
+        rettangolo.setFill(new ImagePattern(img));
+        rettangolo.setX(200);
+        rettangolo.setY(320);
+        //rettangolo.setFill(Color.WHITE);
+        areaDiGioco.getChildren().add(rettangolo);
         
         
         
