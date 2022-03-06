ddbc21247046764544ebd1f4bb27255cb7b2795a
DiegoMariucciScuola <diego.mariucci@iisgubbio.edu.it>
2022-03-06 01:44:20 +0100

    aggiunte tutte le gif e messe nelle cartelle giuste, fatta la camminata di axel

--- src/it/edu/iisgubbio/GameFight/Main.java
+++ src/it/edu/iisgubbio/GameFight/Main.java
@@ -1,18 +1,31 @@
 package it.edu.iisgubbio.GameFight;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 
 public class Main extends Application {
 	Pane areaDiGioco = new Pane();
+	double tempo=0.010;
+	/*
+	 * Verso:
+	 * true=destra
+	 * false=sinistra
+	 */
+	Boolean Verso=true;
+	Boolean staCamminando=false;
 	
+	int contRiposo=0;
 	
 	Rectangle Axel = new Rectangle(180,249);
 	Rectangle Dr = new Rectangle(180,249);
@@ -32,9 +45,16 @@ public class Main extends Application {
 	 * 
 	 */
 	
-	Image AxelRiposo = new Image(getClass().getResourceAsStream("axel-riposoP.gif"));
-	Image DrRiposo = new Image(getClass().getResourceAsStream("dr-riposo.gif"));
 	
+	//riposo
+	Image AxelRiposo = new Image(getClass().getResourceAsStream("axel-riposo.gif"));
+	Image DrRiposo = new Image(getClass().getResourceAsStream("dr-riposo.gif"));
+	Image AxelRiposoInv = new Image(getClass().getResourceAsStream("axel-riposo-inv.gif"));
+	Image DrRiposoInv = new Image(getClass().getResourceAsStream("dr-riposo-inv.gif"));
+	
+	//camminata
+	Image AxelCamminata = new Image(getClass().getResourceAsStream("axel-camminata.gif"));
+	Image AxelCamminataInv = new Image(getClass().getResourceAsStream("axel-camminata-inv.gif"));
 	
 	
 	
@@ -47,7 +67,6 @@ public class Main extends Application {
         sfondo.setPreserveRatio(true);
         sfondo.setFitWidth(1000);
         areaDiGioco.getChildren().add(sfondo);
-        
         
         
         Axel.setFill(new ImagePattern(AxelRiposo));
@@ -65,7 +84,51 @@ public class Main extends Application {
         primaryStage.setScene(scena);
         primaryStage.setTitle("CD");
         primaryStage.show();
+        
+        Timeline timeline = new Timeline(new KeyFrame(
+                Duration.seconds(tempo), 
+                x -> ogniTempo()));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+        scena.setOnKeyPressed(e->movimenti(e));  
 	}
+	private void movimenti(KeyEvent e) {
+		if(e.getText().equals("a")) {
+			Axel.setX(Axel.getX()-24);
+			Verso=false;
+			staCamminando=true;
+			contRiposo=40;
+		}
+		if(e.getText().equals("d")) {
+			Axel.setX(Axel.getX()+24);
+			Verso=true;
+			staCamminando=true;
+			contRiposo=40;
+		}
+		
+	}
+	private void ogniTempo() {
+		
+		if(staCamminando==true) {
+			if(Verso==true) {
+				Axel.setFill(new ImagePattern(AxelCamminata));
+			}else {
+				Axel.setFill(new ImagePattern(AxelCamminataInv));
+			}
+			
+			contRiposo--;
+		}
+		if(contRiposo==0) {
+			staCamminando=false;
+			if(Verso==true) {
+				Axel.setFill(new ImagePattern(AxelRiposo));
+			}else {
+				Axel.setFill(new ImagePattern(AxelRiposoInv));
+			}
+		}
+	}
+	
+	
 	public static void main(String[] args) {
         launch(args);
     }
