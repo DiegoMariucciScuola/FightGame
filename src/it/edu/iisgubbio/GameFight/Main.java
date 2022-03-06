1b4ba665b737d070b766a0aa6b266839a1bffdfa
DiegoMariucciScuola <diego.mariucci@iisgubbio.edu.it>
2022-03-06 15:18:42 +0100

    sistemate altre gif, aggiunta la camminata di Dr, sistemati i limiti e iniziato a programmare il salto

--- src/it/edu/iisgubbio/GameFight/Main.java
+++ src/it/edu/iisgubbio/GameFight/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.ImagePattern;
@@ -22,10 +23,13 @@ public class Main extends Application {
 	 * true=destra
 	 * false=sinistra
 	 */
-	Boolean Verso=true;
-	Boolean staCamminando=false;
+	Boolean VersoAxel=true;
+	Boolean staCamminandoAxel=false;
+	int contRiposoAxel=0;
 	
-	int contRiposo=0;
+	Boolean VersoDr=false;
+	Boolean staCamminandoDr=false;
+	int contRiposoDr=0;
 	
 	Rectangle Axel = new Rectangle(180,249);
 	Rectangle Dr = new Rectangle(180,249);
@@ -55,14 +59,15 @@ public class Main extends Application {
 	//camminata
 	Image AxelCamminata = new Image(getClass().getResourceAsStream("axel-camminata.gif"));
 	Image AxelCamminataInv = new Image(getClass().getResourceAsStream("axel-camminata-inv.gif"));
-	
+	Image DrCamminata = new Image(getClass().getResourceAsStream("dr-camminata.gif"));
+	Image DrCamminataInv = new Image(getClass().getResourceAsStream("dr-camminata-inv.gif"));
 	
 	
 	@Override
     public void start(Stage primaryStage) throws Exception {
         
 		//imposto lo sfondo
-        Image iBase = new Image(getClass().getResourceAsStream("bg1.gif"));
+        Image iBase = new Image(getClass().getResourceAsStream("bg3.gif"));
         ImageView sfondo = new ImageView(iBase);
         sfondo.setPreserveRatio(true);
         sfondo.setFitWidth(1000);
@@ -70,13 +75,13 @@ public class Main extends Application {
         
         
         Axel.setFill(new ImagePattern(AxelRiposo));
-        Axel.setX(200);
-        Axel.setY(320);
+        Axel.setX(20);
+        Axel.setY(340);
         areaDiGioco.getChildren().add(Axel);
         
         Dr.setFill(new ImagePattern(DrRiposo));
-        Dr.setX(600);
-        Dr.setY(320);
+        Dr.setX(800);
+        Dr.setY(340);
         areaDiGioco.getChildren().add(Dr);
         
         
@@ -93,39 +98,90 @@ public class Main extends Application {
         scena.setOnKeyPressed(e->movimenti(e));  
 	}
 	private void movimenti(KeyEvent e) {
+		
+		//prendo i comandi dei movimenti di Axel
 		if(e.getText().equals("a")) {
-			Axel.setX(Axel.getX()-24);
-			Verso=false;
-			staCamminando=true;
-			contRiposo=40;
+			//Axel.setX(Axel.getX()-24);
+			VersoAxel=false;
+			staCamminandoAxel=true;
+			contRiposoAxel=20;
 		}
 		if(e.getText().equals("d")) {
-			Axel.setX(Axel.getX()+24);
-			Verso=true;
-			staCamminando=true;
-			contRiposo=40;
+			//Axel.setX(Axel.getX()+24);
+			VersoAxel=true;
+			staCamminandoAxel=true;
+			contRiposoAxel=20;
 		}
 		
+		//prendo i comandi dei movimenti di Dr
+		if(e.getCode()==KeyCode.LEFT) {
+			//Dr.setX(Dr.getX()-24);
+			VersoDr=false;
+			staCamminandoDr=true;
+			contRiposoDr=20;
+		}
+		if(e.getCode()==KeyCode.RIGHT) {
+			//Dr.setX(Dr.getX()+24);
+			VersoDr=true;
+			staCamminandoDr=true;
+			contRiposoDr=20;
+		}
 	}
 	private void ogniTempo() {
 		
-		if(staCamminando==true) {
-			if(Verso==true) {
+		
+		//regolo il movimento, il verso e quando smettere di camminare di axel
+		if(staCamminandoAxel==true) {
+			if(VersoAxel==true) {
 				Axel.setFill(new ImagePattern(AxelCamminata));
+				if((Axel.getX()+180)<Dr.getX()||Axel.getX()>(Dr.getX())+180) {
+					Axel.setX(Axel.getX()+3);
+				}
 			}else {
 				Axel.setFill(new ImagePattern(AxelCamminataInv));
+				if((Axel.getX()+180)<Dr.getX()||Axel.getX()>(Dr.getX())+180) {
+					Axel.setX(Axel.getX()-3);
+				}
 			}
 			
-			contRiposo--;
+			contRiposoAxel--;
 		}
-		if(contRiposo==0) {
-			staCamminando=false;
-			if(Verso==true) {
+		if(contRiposoAxel==0) {
+			staCamminandoAxel=false;
+			if(VersoAxel==true) {
 				Axel.setFill(new ImagePattern(AxelRiposo));
 			}else {
 				Axel.setFill(new ImagePattern(AxelRiposoInv));
 			}
 		}
+		
+		//regolo il movimento, il verso e quando smettere di camminare di Dr
+		if(staCamminandoDr==true) {
+			if(VersoDr==true) {
+				Dr.setFill(new ImagePattern(DrCamminata));
+				if((Dr.getX()+180)<Axel.getX()||Dr.getX()>(Axel.getX())+180) {
+					Dr.setX(Dr.getX()+3);
+				}
+				//Dr.setX(Dr.getX()+3);
+			}else {
+				Dr.setFill(new ImagePattern(DrCamminataInv));
+				if((Dr.getX()+180)<Axel.getX()||Dr.getX()>(Axel.getX())+180) {
+					Dr.setX(Dr.getX()-3);
+				}
+				//Dr.setX(Dr.getX()-3);
+			}
+			contRiposoDr--;
+		}
+		if(contRiposoDr==0) {
+			staCamminandoDr=false;
+			if(VersoDr==true) {
+				Dr.setFill(new ImagePattern(DrRiposo));
+			}else {
+				Dr.setFill(new ImagePattern(DrRiposoInv));
+			}
+		}
+		
+		
 	}
 	
 	
